ebcd0b4ced260aac35fdcd6fb419d3e8ada2b776
S <202709792+Syntax242@users.noreply.github.com>
2025-11-21 03:40:55 +0300

    Version 0.2

--- cfg.lua
+++ cfg.lua
@@ -1,10 +1,31 @@
 Config = {}
 
+------VEHICLE RADIO------
+
+
 -- Turn off the in-vehicle radio completely
 Config.DisableVehicleRadio = true
+
+
+------PUNCH REQUIRES FOCUS------
+
 
 -- Allows punching only when the player is targeting something
 Config.PunchRequiresFocus = true
 
+
+------AMBIENT SOUNDS------
+
+
 -- Disables ambient world sounds such as background noise and police scanner
-Config.DisableAmbientSounds = true
+Config.DisableAmbientSounds = false
+
+
+------BUNNYHOPPING------
+
+
+Config.EnableBunnyhopping = false
+-- How many jumps to trigger the ragdoll
+Config.RequiredJumps = 3
+-- Ragdoll time (ms)
+Config.RagdollTime = 3000
--- cl.lua
+++ cl.lua
@@ -1,32 +1,24 @@
 CreateThread(function()
+    if not Config.DisableVehicleRadio then return end
     while true do
         Wait(1000)
-
-        if Config.DisableVehicleRadio then
-            local ped = PlayerPedId()
-
-            if IsPedInAnyVehicle(ped, false) then
-                SetUserRadioControlEnabled(false)
-                if GetPlayerRadioStationName() ~= nil then
-                    SetVehRadioStation(GetVehiclePedIsIn(ped), "OFF")
-                end
+        local ped = PlayerPedId()
+        if IsPedInAnyVehicle(ped, false) then
+            SetUserRadioControlEnabled(false)
+            if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(ped), "OFF")
             end
-        else
-            SetUserRadioControlEnabled(true)
         end
     end
 end)
 CreateThread(function()
+    if not Config.PunchRequiresFocus then return end
     while true do
         Wait(0)
-
-        if Config.PunchRequiresFocus then
-            DisableControlAction(1, 140, true)
-
-            if not IsPlayerTargettingAnything(PlayerId()) then
-                DisableControlAction(1, 141, true)
-                DisableControlAction(1, 142, true)
-            end
+        DisableControlAction(1, 140, true)
+        if not IsPlayerTargettingAnything(PlayerId()) then
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
         end
     end
 end)
@@ -36,3 +28,37 @@ CreateThread(function()
         SetAudioFlag("PoliceScannerDisabled", true)
     end
 end)
+local jumpCount = 0
+local lastJumpTime = 0
+local isJumping = false
+local jumpWindow = 10000 
+CreateThread(function()
+    if not Config.EnableBunnyhopping then return end
+    while true do
+        Wait(500)
+        local ped = PlayerPedId()
+        if IsPedJumping(ped) then
+            if not isJumping then
+                isJumping = true
+                local now = GetGameTimer()
+                if (now - lastJumpTime) <= jumpWindow then
+                    jumpCount = jumpCount + 1
+                else
+                    jumpCount = 1
+                end
+                lastJumpTime = now
+                if jumpCount >= Config.RequiredJumps then
+                    TriggerEvent("threejump:ragdoll")
+                    jumpCount = 0
+                end
+            end
+        else
+            isJumping = false
+        end
+    end
+end)
+RegisterNetEvent("threejump:ragdoll", function()
+    local ped = PlayerPedId()
+    SetPedToRagdoll(ped, Config.RagdollTime, Config.RagdollTime, 0, false, false, false)
+end)
+
